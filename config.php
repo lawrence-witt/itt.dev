10e8873881130d45ff8c971818703bbe8f0d9df8
Lawrence Witt <55201111+lawrence-witt@users.noreply.github.com>
2020-11-08 12:58:48 +0000

    Reset BASE_URL

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
     const IMG_DIR = "./dist/imgs/";
     const JS_DIR = "./dist/js/";
     const CSS_DIR = "./dist/css/";
-    const BASE_URL = "http://localhost:8500";
+    const BASE_URL = "https://itt.dev";
 ?>
